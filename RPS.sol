075e4d338c777fc84069aefb4e0c666ecb181c6b
pozzy21 <pozzy199911@gmail.com>
2023-03-27 23:51:28 +1000

    Let's goo

--- RPS.sol
+++ RPS.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract RockPaperScissors{
+contract RockPaperScissors {
     //modifier onlyOwner
-    modifier onlyOwner{
+    modifier onlyOwner() {
         require(msg.sender == owner);
         _;
     }
 
     //Owner's address
-    address owner; 
+    address owner;
 
     //event to track result of Coin Flip
-    event Game(address player, uint256 amount, uint8 option, uint8 result); 
+    event Game(address player, uint256 amount, uint8 option, uint8 result);
 
     //in Constructor we assign owner's address;
     constructor() payable {
@@ -20,52 +20,49 @@ contract RockPaperScissors{
     }
 
     //function that asks for 0 or 1 and returns if you win or lose
-    function RockPaperScissor(uint8 _option) public payable returns (bool){ //view, pure = gassless 
-        require(_option<3, "Please select 0,1 or 2");
-        require(msg.value>0, "Please add your bet"); //WEI smallest unit ETH 
-        //1,000,000,000,000,000,000 WEI = 1 ETH 
-        require(msg.value*2 <= address(this).balance, "Contract balance is insuffieient ");
+    function RockPaperScissor(uint8 _option) public payable returns (bool) {
+        //view, pure = gassless
+        require(_option < 3, "Please select 0,1 or 2");
+        require(msg.value > 0, "Please add your bet"); //WEI smallest unit ETH
+        //1,000,000,000,000,000,000 WEI = 1 ETH
+        require(
+            msg.value * 2 <= address(this).balance,
+            "Contract balance is insuffieient "
+        );
 
-        //PseudoRandom and check with _option, return 0,1 or 2 
-        uint256 random = (block.timestamp*block.gaslimit)%3;
+        //PseudoRandom and check with _option, return 0,1 or 2
+        uint256 random = (block.timestamp * block.gaslimit) % 3;
         uint8 result;
         //TODO add oracle: https://docs.chain.link/vrf/v2/introduction
         //1 - rock 2 - paper - scissor
         emit Game(msg.sender, msg.value, _option, result);
         //hardcoded 0 - draw 1 - lose 2 - win
-        if(random == _option){
+        if (random == _option) {
             result = 0;
+        } else if (random == 0 && _option == 1) {
+            result = 1;
+        } else if (random == 0 && _option == 2) {
+            result = 2;
+        } else if (random == 1 && _option == 0) {
+            result = 2;
+        } else if (random == 1 && _option == 2) {
+            result = 1;
+        } else if (random == 2 && _option == 0) {
+            result = 1;
+        } else if (random == 2 && _option == 1) {
+            result = 2;
         }
-        else if (random == 0 && _option == 1) {
-            result =  1;
-            }
-        else if (random == 0 && _option == 2) { 
-            result =  2;
-            }
-        else if (random == 1 && _option == 0) { 
-            result =  2;
-        }
-        else if (random == 1 && _option == 2) {
-            result =  1;
-            }
-
-        else if (random == 2 && _option == 0) {
-            result =  1;
-        }
-        else if (random == 2 && _option == 1) {
-            result =  2;
-         }
-        if (result == 2){
+        if (result == 2) {
             //winner winner chicken dinner
-            payable(msg.sender).transfer(msg.value*2);
+            payable(msg.sender).transfer(msg.value * 2);
             return true;
         }
         //lose
-        else if (result == 1){
+        else if (result == 1) {
             return false;
-            }
+        }
         //draw
-        else{
+        else {
             payable(msg.sender).transfer(msg.value);
             return false;
         }
@@ -75,5 +72,4 @@ contract RockPaperScissors{
     function withdraw() public onlyOwner {
         payable(msg.sender).transfer(address(this).balance);
     }
-
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const contractAddress = "0x8613BE02E18e637726551d3588754946Bb91a4DB";
+const contractAddress = "0xcf6d4163DBcaA4C5F0eCD43DAF2a0d33f46AD09A";
 const contractABI = [
 	{
 		"inputs": [],
@@ -80,7 +80,6 @@ provider.send("eth_requestAccounts", []).then(()=>{
             contractABI,
             signer
         )
-     
     }
     )
 }
@@ -89,15 +88,15 @@ provider.send("eth_requestAccounts", []).then(()=>{
 async function flipCoin(_option){
     let amountInEth = document.getElementById("amountInEth").value;
     let amountInWei = ethers.utils.parseEther(amountInEth.toString())
-    console.log(amountInWei);
+    //console.log(amountInWei);
     
     let resultOfCoinFlip = await contract.RockPaperScissor(_option, {value: amountInWei});
     const res = await resultOfCoinFlip.wait();
     console.log(res);
-    //console.log( await res.events[0].args.player.toString());
+    //console.log(await res.events[0].args.player.toString());
     
 
-    let queryResult =  await contract.queryFilter('Game', await provider.getBlockNumber() - 10000, await provider.getBlockNumber());
+    let queryResult =  await contract.queryFilter('Game', await provider.getBlockNumber() - 100, await provider.getBlockNumber());
     let queryResultRecent = queryResult[queryResult.length-1]
     //console.log(queryResult[queryResult.length-1].args);
 
@@ -106,37 +105,69 @@ async function flipCoin(_option){
     let option = await queryResultRecent.args.option.toString();
     let result = await queryResultRecent.args.result.toString();
 
+
+	alert(result);
     let resultLogs = `
     stake amount: ${ethers.utils.formatEther(amount.toString())} BNB, 
     player: ${player}, 
-    player chose: ${option ==0 ? "HEAD": "TAIL"}, 
-    result: ${result == false ? "LOSE 😥": "WIN 🎉"}`;
+	${result == 2 ? "result: WIN 😥":""}
+	${result == 1 ? "result: LOSE 😥":""}
+	${result == 0 ? "result: DRAW 😥":""}`
     console.log(resultLogs);
+
+
+
+
 
     let resultLog = document.getElementById("resultLog");
     resultLog.innerText = resultLogs;
 
     handleEvent();
+	sendMessage();
 }
 
 async function handleEvent(){
 
-    //console.log(await contract.filters.CoinFlipped());
-    let queryResult =  await contract.queryFilter('Game', await provider.getBlockNumber() - 10000, await provider.getBlockNumber());
+    console.log(await contract.filters.Game());
+    let queryResult =  await contract.queryFilter('Game', await provider.getBlockNumber() - 100, await provider.getBlockNumber());
     let queryResultRecent = queryResult[queryResult.length-1]
     let amount = await queryResultRecent.args.amount.toString();
     let player = await queryResultRecent.args.player.toString();
     let option = await queryResultRecent.args.option.toString();
     let result = await queryResultRecent.args.result.toString();
-
     let resultLogs = `
     stake amount: ${ethers.utils.formatEther(amount.toString())} BNB, 
     player: ${player}, 
-    player chose: ${option ==0 ? "HEAD": "TAIL"}, 
-    result: ${result == false ? "LOSE 😥": "WIN 🎉"}`;
+     ${result == 2 ? "result: WIN 😥":""}
+	 ${result == 1 ? "result: LOSE 😥":""}
+	 ${result == 0 ? "result: DRAW 😥":""}`
     console.log(resultLogs);
+
+
+
+
+
 
     let resultLog = document.getElementById("resultLog");
     resultLog.innerText = resultLogs;
-    
 }
+
+
+async function sendMessage() {
+	let queryResult =  await contract.queryFilter('Game', await provider.getBlockNumber() - 100, await provider.getBlockNumber());
+    let queryResultRecent = queryResult[queryResult.length-1]
+    let amount = await queryResultRecent.args.amount.toString();
+    let player = await queryResultRecent.args.player.toString();
+    let option = await queryResultRecent.args.option.toString();
+    let result = await queryResultRecent.args.result.toString();
+    let resultLogs = `
+    stake amount: ${ethers.utils.formatEther(amount.toString())} BNB, 
+    player: ${player}, 
+	${result == 2 ? "result: WIN 😥":""}
+	${result == 1 ? "result: LOSE 😥":""}
+	${result == 0 ? "result: DRAW 😥":""}`
+
+	  let game = document.getElementById("games");
+	  game.innerHTML += "<p>" + resultLogs + "</p>";
+	
+  }
